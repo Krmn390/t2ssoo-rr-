f6cc81b4c3e26faf1ee6a33f19b30d91df6baf64
FraMqueli <flmontalba@uc.cl>
2024-10-15 18:29:38 -0300

    structs end?

--- src/osrms_API/osrms_API.c
+++ src/osrms_API/osrms_API.c
@@ -29,6 +29,22 @@ typedef struct tabla_archs{
 } Tabla_Arch;
 
 typedef struct tabla_pag_1_ord{
-    entrada_prim_ord[64]
-    
-}
+    int16_t entrada_pag_prim_ord[64];
+} Tabla_Pag_Prim_Ord;
+
+typedef struct espacio_tablas_2do_orden{
+    unsigned tamano; //128000 bytes = 128KB
+    char tabla_pagina_2do_orden; // 128 bytes
+} Espacio_Tablas_2do_Orden;
+
+
+typedef struct bit_map_tabla{
+    bool bitmap[128] ; // libre false, ocupada true
+} Bit_map;
+
+typedef struct tabla_pags_2do_orden{   // Relativas a los últimos 2Gb de memoria 
+    int dir;  // La dirección absoluta es 8 * 2^10 + 128 + 128 * 2^10 + 8 * 2^10 + dir 
+    uint16_t entradas_2do_orden[64];
+} Tabla_Pags_2do_Orden;
+
+
